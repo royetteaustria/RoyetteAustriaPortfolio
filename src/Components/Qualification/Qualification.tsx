5566b005bb1823f18ffa71c3f80791b78d538681
royetteaustria <austriaroyette@gmail.com>
2026-04-23 19:32:28 +0800

    Add Experience in Qualification

--- src/Components/Qualification/Qualification.tsx
+++ src/Components/Qualification/Qualification.tsx
@@ -7,7 +7,6 @@ const Qualification = () => {
 
       <div className="relative mx-auto mt-12 flex w-full flex-col lg:w-2/3">
         <span className="left-2/5 absolute inset-y-0 ml-10 hidden w-0.5 bg-grey-40 md:block"></span>
-
         <div className="mt-8 flex flex-col text-center md:flex-row md:text-left">
           <div className="md:w-2/5">
             <div className="flex justify-center md:justify-start">
@@ -18,6 +17,39 @@ const Qualification = () => {
                 <span className="absolute inset-x-0 top-1/2 h-0.5 -translate-y-1/2 transform bg-grey-70"></span>
               </div>
             </div>
+          </div>
+          <div className="md:w-3/5">
+            <div className="relative flex md:pl-18">
+              <span className="absolute left-8 top-1 hidden h-4 w-4 rounded-full border-2 border-grey-40 bg-white md:block"></span>
+
+              <div className="mt-1 flex">
+                <i className="bx bxs-right-arrow hidden text-primary md:block"></i>
+                <div className="md:-mt-1 md:pl-8">
+                  <span className="block font-body font-bold text-grey-40">
+                    May 2025 (Current)
+                  </span>
+                  <span className="block pt-2 font-header text-xl font-bold uppercase text-primary">
+                    IT Staff
+                  </span>
+                  <span className="block font-body font-bold text-grey-40">
+                  Epson Precision Philippines Inc
+                  </span>
+                  <div className="pt-2">
+                    <span className="block font-body text-black">
+                      {" "}
+                      • Creates databases and data marts for <b>Power BI</b> reporting using Microsoft <b>SQL Server</b> <br />
+                      • Designs and develops internal business applications using <b>Microsoft Power Apps</b><br />
+                      • Automates workflows and business processes with <b>Power Automate</b><br />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-8 flex flex-col text-center md:flex-row md:text-left">
+          <div className="md:w-2/5">
+            {/* Intern Experience */}
           </div>
           <div className="md:w-3/5">
             <div className="relative flex md:pl-18">
@@ -38,7 +70,7 @@ const Qualification = () => {
                   <div className="pt-2">
                     <span className="block font-body text-black">
                       {" "}
-                      Maintain the company website using WORDPRESS and created
+                      Maintain the company website using <b>WORDPRESS</b> and created
                       graphic designs for learning materials
                     </span>
                   </div>
